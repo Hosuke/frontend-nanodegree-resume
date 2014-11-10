363ff0acdfd6cfe09394a9df98236f868460e954
Geyang <babxyq@gmail.com>
2014-11-11 00:58:46 +1100

    Fix internationalize

--- js/helper.js
+++ js/helper.js
@@ -16,17 +16,17 @@ var HTMLheaderName = "<h1 id='name'>%data%</h1>";
 var HTMLheaderRole = "<span>%data%</span><hr/>";
 
 var HTMLcontactGeneric = "<li class='flex-item'><span class='orange-text'>%contact%</span><span class='white-text'>%data%</span></li>";
-var HTMLmobile = "<li class='flex-item'><span class='orange-text'>mobile</span><span class='white-text'>%data%</span></li>";
-var HTMLemail = "<li class='flex-item'><span class='orange-text'>email</span><span class='white-text'>%data%</span></li>";
-var HTMLtwitter = "<li class='flex-item'><span class='orange-text'>twitter</span><span class='white-text'>%data%</span></li>";
-var HTMLgithub = "<li class='flex-item'><span class='orange-text'>github</span><span class='white-text'>%data%</span></li>";
-var HTMLblog = "<li class='flex-item'><span class='orange-text'>blog</span><span class='white-text'>%data%</span></li>";
-var HTMLlocation = "<li class='flex-item'><span class='orange-text'>location</span><span class='white-text'>%data%</span></li>";
+var HTMLmobile = "<li class='flex-item col-md-3'><span class='orange-text'>mobile</span><span class='white-text'>%data%</span></li>";
+var HTMLemail = "<li class='flex-item col-md-3'><span class='orange-text'>email</span><span class='white-text'>%data%</span></li>";
+var HTMLtwitter = "<li class='flex-item col-md-3'><span class='orange-text'>twitter</span><span class='white-text'>%data%</span></li>";
+var HTMLgithub = "<li class='flex-item col-md-3'><span class='orange-text'>github</span><span class='white-text'>%data%</span></li>";
+var HTMLblog = "<li class='flex-item col-md-3'><span class='orange-text'>blog</span><span class='white-text'>%data%</span></li>";
+var HTMLlocation = "<li class='flex-item col-md-3'><span class='orange-text'>location</span><span class='white-text'>%data%</span></li>";
 
 var HTMLbioPic = "<img src='%data%' class='biopic'>";
 var HTMLWelcomeMsg = "<span class='welcome-message'>%data%</span>";
 
-var HTMLskillsStart = "<h3 id='skillsH3'>Skills at a Glance:</h3><ul id='skills' class='flex-box'></ul>";
+var HTMLskillsStart = "<h3 id='skills'>Skills at a Glance:</h3><ul id='skills' class='flex-box'></ul>";
 var HTMLskills = "<li class='flex-item'><span class='white-text'>%data%</span></li>";
 
 var HTMLworkStart = "<div class='work-entry'></div>";
@@ -64,7 +64,7 @@ The International Name challenge in Lesson 2 where you'll create a function that
 */
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName() || function(){};
+    var iName = inName(bio.name) || function(){};
     $('#name').html(iName);
   });
 })
